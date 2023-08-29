3554c3cd02a98250bd251e253cd1e38a9d043bf3
yuriyshchekotov <yuriyshchekotov@gmail.com>
2023-08-29 23:01:47 +0500

    New lesson

---
+++ src/Test.java
@@ -0,0 +1,22 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args){
+        String[] var_arrayComplex = new String[] {"one", "two", "three"};
+        int[] var_arrayShort = {1, 2, 3};
+        System.out.println("Should I count Latin or Arabic?");
+        Scanner var_scanner = new Scanner(System.in);
+        String var_answer = var_scanner.nextLine();
+        if(Objects.equals(var_answer.toLowerCase(), "arabic"))
+            for(int a: var_arrayShort) {
+            System.out.println(a + " Mississippi");
+        }
+        if(Objects.equals(var_answer.toLowerCase(), "latin"))
+            for(String var_val : var_arrayComplex){
+                System.out.println(var_val + " Mississippi");
+            }
+        if((!Objects.equals(var_answer.toLowerCase(), "latin") & !Objects.equals(var_answer.toLowerCase(), "arabic")) )
+            System.out.println("Don't understand you, bye!");
+    }
+}
--- src/TestArray.java
+++
@@ -1,8 +0,0 @@
-public class TestArray {
-    public static void main(String[] args){
-        int[] var_array = {1, 2, 3, 4, 5};
-        for(int a: var_array){
-            System.out.println(a + " Mississippi");
-        }
-    }
-}
